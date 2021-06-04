1cfacd1fc270378b9d85b42e44846a4e905143ae
Stas Panasiuk <temnyip@gmail.com>
2021-06-04 16:57:06 +0300

    implemented the balances calculation

--- src/main/java/org/meme/corp/util/GraphUtil.java
+++ src/main/java/org/meme/corp/util/GraphUtil.java
@@ -1,12 +1,19 @@
 package org.meme.corp.util;
 
-import java.util.Map;
-
 public final class GraphUtil {
 
-  //TODO
-  public static Map<Integer, Float> calculateBalances(float[][] expenses) {
-    return null;
+  public static float[] calculateBalances(float[][] expenses) {
+    final float[] result = new float[expenses.length];
+    for (int col = 0; col < expenses[0].length; col++) {
+      for (int row = 0; row < expenses.length; row++) {
+        if (col == row) {
+          continue;
+        }
+        result[col] -= expenses[row][col];
+        result[row] += expenses[row][col];
+      }
+    }
+    return result;
   }
 
 }
