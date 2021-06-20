fc0c2bc219866f74a6da8750c5b4d53cb3ae4c67
Maksym Kazarin <kazzarin99@gmail.com>
2021-06-21 01:26:50 +0300

    Flyway location path was reworked

--- src/test/java/org/meme/corp/database/repository/AbstractRepositoryTest.java
+++ src/test/java/org/meme/corp/database/repository/AbstractRepositoryTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.meme.corp.database.repository.configuration.PostgresqlContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import java.io.File;
+
 public class AbstractRepositoryTest {
 
-    private static final String MIGRATIONS_LOCATION = "classpath:db/migrations";
+    private static final String MIGRATIONS_LOCATION = "filesystem:" + System.getProperty("user.dir") + File.separator + String.join(File.separator, "src", "main", "resources", "db", "migrations");
     private static final String MIGRATIONS_SCHEMAS = "public";
 
     private static final PostgreSQLContainer<PostgresqlContainer> postgreSQLContainer = PostgresqlContainer.getNewInstance();
