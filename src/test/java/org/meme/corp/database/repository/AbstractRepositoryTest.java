d935dd678bf1951006f8a480a9ddf00d9dc7f9e1
Maksym Kazarin <kazzarin99@gmail.com>
2021-06-21 01:05:17 +0300

    Adding classpath to flyway migration

--- src/test/java/org/meme/corp/database/repository/AbstractRepositoryTest.java
+++ src/test/java/org/meme/corp/database/repository/AbstractRepositoryTest.java
@@ -2,14 +2,12 @@ package org.meme.corp.database.repository;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.meme.corp.database.repository.configuration.PostgresqlContainer;
-import org.meme.corp.database.repository.impl.PersonClientRepository;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public class AbstractRepositoryTest {
 
-    private static final String MIGRATIONS_LOCATION = "db/migrations";
+    private static final String MIGRATIONS_LOCATION = "classpath:db/migrations";
     private static final String MIGRATIONS_SCHEMAS = "public";
 
     private static final PostgreSQLContainer<PostgresqlContainer> postgreSQLContainer = PostgresqlContainer.getNewInstance();
